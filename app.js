ef65e930a41e1a43efdd17ec5d70cada6c4b19ef
Amelia Gianoli <gianolia@students.westerntc.edu>
2026-02-23 11:32:09 -0600

    Completed Step 3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 8080; // We'll run the server on port 8080
 const express = require('express');
 const app = express();
 const bodyParser=require('body-parser');
+const {getBlogList, convertMarkdown} = require("./modules/markdown-helpers")
+const pathToBlogFolder = __dirname + '/blog/';
 
 // MIDDLEWARE
 app.use(express.static('public'));
@@ -13,35 +15,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // ROUTES
+
 // Home:
 app.get('/', (req, res) => {
    res.render('home', {
       title: "My Home Page",
    });
 });
+
 // Blog List
 app.get('/blog', (req, res)=>{
+  const blogList = getBlogList(pathToBlogFolder);
   res.render('blog-list', {
-    title: "Blog List"
+    title: "Blog",
+    posts: blogList
   });
 });
+
 //  Blog Post
 app.get("/blog/:post", (req, res) => {
-  console.log("The :post param is set to: " +  req.params.post);
-  res.render("blog-post", {
-    title: "Some Title",
-    description: "Some Description",
-    author: "Some Author",
-    published: "Some Date",
-    content: "Some content..."
-  });
+  try{
+    const pathToFile = pathToBlogFolder + req.params.post + ".md";
+    console.log("Markdown file: " + pathToFile);
+    const obj = convertMarkdown(pathToFile);
+    res.render('blog-post', {
+       title: obj.data.title,
+       description: obj.data.description,
+       author: obj.data.author,
+       published: obj.data.published,
+       content: obj.html
+    });
+  }catch(error){
+    console.log(error);
+    res.status(404).redirect("/404");
+  }
 });
+
 //  Contact page
 app.get('/contact', (req, res) => {
   res.render('contact', {
      title: "Contact Me"
   });
 });
+
 //  Contact POST
 app.post('/contact/submit', (req, res) => {
   res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
