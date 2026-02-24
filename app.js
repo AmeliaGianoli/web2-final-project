d8bfd80257b1141ce1f904e72331230fb02e2fa5
Amelia Gianoli <gianolia@students.westerntc.edu>
2026-02-24 15:59:07 -0600

    step 5 complete

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
+// Load environment variables FIRST
+require('dotenv').config();
+
 const port = 8080; // We'll run the server on port 8080
 
 // IMPORTS
 const express = require('express');
 const app = express();
-const bodyParser=require('body-parser');
-const {getBlogList, convertMarkdown} = require("./modules/markdown-helpers")
+const bodyParser = require('body-parser');
+const { getBlogList, convertMarkdown } = require("./modules/markdown-helpers")
 const pathToBlogFolder = __dirname + '/blog/';
+
 
 // MIDDLEWARE
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 // allow the app to get data for form submits
 app.use(bodyParser.urlencoded({ extended: true }));
-
+// Redirect to HTTPS
+app.use((req, res, next) => {
+   if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] !== 'https')
+         // the statement for performing our redirection
+         return res.redirect('https://' + req.headers.host + req.url);
+      else
+         return next();
+   }else{
+      return next();
+   }
+});
 
 // ROUTES
 
 // Home:
 app.get('/', (req, res) => {
-   res.render('home', {
-      title: "My Home Page",
-   });
+  res.render('home', {
+    title: "My Home Page",
+  });
 });
 
 // Blog List
-app.get('/blog', (req, res)=>{
+app.get('/blog', (req, res) => {
   const blogList = getBlogList(pathToBlogFolder);
   res.render('blog-list', {
     title: "Blog",
@@ -34,18 +49,18 @@ app.get('/blog', (req, res)=>{
 
 //  Blog Post
 app.get("/blog/:post", (req, res) => {
-  try{
+  try {
     const pathToFile = pathToBlogFolder + req.params.post + ".md";
     console.log("Markdown file: " + pathToFile);
     const obj = convertMarkdown(pathToFile);
     res.render('blog-post', {
-       title: obj.data.title,
-       description: obj.data.description,
-       author: obj.data.author,
-       published: obj.data.published,
-       content: obj.html
+      title: obj.data.title,
+      description: obj.data.description,
+      author: obj.data.author,
+      published: obj.data.published,
+      content: obj.html
     });
-  }catch(error){
+  } catch (error) {
     console.log(error);
     res.status(404).redirect("/404");
   }
@@ -54,21 +69,49 @@ app.get("/blog/:post", (req, res) => {
 //  Contact page
 app.get('/contact', (req, res) => {
   res.render('contact', {
-     title: "Contact Me"
+    title: "Contact Me"
   });
 });
 
 //  Contact POST
 app.post('/contact/submit', (req, res) => {
-  res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
+
+  // import the helper functions that we need
+  const { isValidContactFormSubmit, sendEmailNotification } = require("./modules/contact-helpers");
+
+  // Destructure the req.body object into variables
+  const { firstName, lastName, email, comments } = req.body;
+
+  // Validate the variables
+  if (isValidContactFormSubmit(firstName, lastName, email, comments)) {
+    // Everything is valid, so send an email to YOUR email address with the data entered into the form
+    const message = `From: ${firstName} ${lastName}\n
+                    Email: ${email}\n
+                    Message: ${comments}`;
+
+    sendEmailNotification(message, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("There was an error sending the email");
+      } else {
+        // Render a template that confirms the contact form info was recieved:
+        res.render("default-layout", {
+          title: "Contact Confirmation",
+          content: "<h2>Thank you for contacting me!</h2><p>I'll get back to you ASAP.</p>"
+        })
+      }
+    });
+  } else {
+    res.status(400).send("Invalid request - data is not valid")
+  }
 });
 
 // 404 Not Found
 app.get("/404", (req, res) => {
   res.status(404);
   res.render('default-layout', {
-     title: "Page Not Found",
-     content: "<h1>Sorry!</h1><h3>We can't find the page you're requesting.</h3>"
+    title: "Page Not Found",
+    content: "<h1>Sorry!</h1><h3>We can't find the page you're requesting.</h3>"
   });
 });
 
@@ -77,11 +120,11 @@ app.get("/404", (req, res) => {
 
 // CATCH-ALL
 // WARNING: Must be final code in ROUTE section!
-app.all('*', (req, res)=>{
+app.all('/*path', (req, res) => {
   res.status(404).redirect("/404");
 });
 
 // START THE SERVER
 const server = app.listen(port, () => {
-   console.log("Waiting for requests on port %s", port);
+  console.log("Waiting for requests on port %s", port);
 });
